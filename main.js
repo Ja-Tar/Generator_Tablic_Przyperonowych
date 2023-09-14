39192a4ca41e48cd32f2d8e32c59214dd6e4e643
Jan Tarnogórski <tarnogorskijan@gmail.com>
2023-09-14 19:32:14 +0200

    Chyba trochę dodałem :)
    Zmiana wyglądu + Nowe pole "Nazwa" + zero stacji pośrednich

--- main.js
+++ main.js
@@ -5,25 +5,42 @@ function generujStrone() {
     var stacja_koncowa = document.getElementById('stacja_koncowa').value;
     var stacje_posrednie = document.getElementById('stacje_posrednie').value;
     var przewoznik = document.getElementById('przewoznik').value;
+    var nazwa = document.getElementById('nazwapociagu').value;
 
     // Pobierz treść szablonu z pliku ekran_tamplate.html za pomocą fetch
     fetch('ekran_template.html')
         .then(response => response.text())
         .then(template => {
             // Sprawdzamy czy wszystkie pola są wypełnione
-            if (godz === '' || numer_pociagu === '' || stacja_koncowa === '' || stacje_posrednie === '' || przewoznik === '') {
-                alert('Proszę wypełnić wszystkie pola!');
+            if (godz === '' || numer_pociagu === '' || stacja_koncowa === '' || przewoznik === '') {
+                alert('Proszę wypełnić wszystkie wymagane pola!');
                 return;
             }
 
             // Wypełnij szablon danymi
-            var wypelnionySzablon = template.replace(/{godz}/g, godz).replace(/{numer_pociagu}/g, numer_pociagu).replace(/{stacja_koncowa}/g, stacja_koncowa).replace(/{stacje_posrednie}/g, stacje_posrednie).replace(/{przewoznik}/g, przewoznik);
+            var wypelnionySzablon = template.replace(/{godz}/g, godz)
+                .replace(/{numer_pociagu}/g, numer_pociagu)
+                .replace(/{stacja_koncowa}/g, stacja_koncowa)
+                .replace(/{stacje_posrednie}/g, stacje_posrednie)
+                .replace(/{przewoznik}/g, przewoznik)
+                .replace(/{nazwa}/g, nazwa);
 
             // Wstaw wypełniony szablon do dokumentu
             var miejsceNaStrone = document.getElementById('wygenerowanaStrona');
             miejsceNaStrone.innerHTML = wypelnionySzablon;
+            
+            // Hide the elements with IDs 'nazwapociagutext' and 'nazwapociagu' if 'nazwa' is empty
+            if (nazwa === '') {
+                var nazwaText = document.getElementById('tp_nazwapociagutext');
+                var nazwaDiv = document.getElementById('tp_nazwapociagu');
+                nazwaText.style.display = 'none';
+                nazwaDiv.style.display = 'none';
+                var przewoznikDiv = document.querySelector('.przewoznik');
+                przewoznikDiv.classList.add('move-right');
+            }
+            
             var miejsceNaZdjecie = document.getElementById('wygenerowaneZdjecie');
-            html2canvas(document.getElementById('wygenerowanaStrona'), {width: 900}).then(function(canvas) {
+            html2canvas(document.getElementById('wygenerowanaStrona'), { width: 900 }).then(function (canvas) {
                 miejsceNaZdjecie.innerHTML = "";
                 var img = new Image();
                 img.src = canvas.toDataURL("image/png");
@@ -32,7 +49,7 @@ function generujStrone() {
                 miejsceNaZdjecie.append(img);
             });
             var classa = document.getElementById('boxtekst');
-            classa.classList.add("animacja-migmig")
+            classa.classList.add("animacja-migmig");
         })
         .catch(error => {
             console.error('Błąd pobierania szablonu:', error);
