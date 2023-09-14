43f7baade8c3dfccc073485fd9db58738a56b5b9
Jan Tarnogórski <tarnogorskijan@gmail.com>
2023-09-14 20:56:05 +0200

    Ok, to by było tyle
    Zmiana na 2 przyciski, generuj i generuj ze zdjęciem, dodanie przewijania

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-function generujStrone() {
+function generujStrone(zdjecie = false) {
     // Pobieramy wszystkie dane wprowadzone do formularza
     var godz = document.getElementById('godz').value;
     var numer_pociagu = document.getElementById('numer_pociagu').value;
-    var stacja_koncowa = document.getElementById('stacja_koncowa').value;
+    var stacja_koncowa = document.getElementById('stacja_koncowa').value.replace(/ /g, '&nbsp;');
     var stacje_posrednie = document.getElementById('stacje_posrednie').value;
     var przewoznik = document.getElementById('przewoznik').value;
     var nazwa = document.getElementById('nazwapociagu').value;
@@ -28,7 +28,9 @@ function generujStrone() {
             // Wstaw wypełniony szablon do dokumentu
             var miejsceNaStrone = document.getElementById('wygenerowanaStrona');
             miejsceNaStrone.innerHTML = wypelnionySzablon;
+
             
+
             // Hide the elements with IDs 'nazwapociagutext' and 'nazwapociagu' if 'nazwa' is empty
             if (nazwa === '') {
                 var nazwaText = document.getElementById('tp_nazwapociagutext');
@@ -38,18 +40,40 @@ function generujStrone() {
                 var przewoznikDiv = document.querySelector('.przewoznik');
                 przewoznikDiv.classList.add('move-right');
             }
-            
-            var miejsceNaZdjecie = document.getElementById('wygenerowaneZdjecie');
-            html2canvas(document.getElementById('wygenerowanaStrona'), { width: 900 }).then(function (canvas) {
+
+            if (zdjecie === true) {
+                var miejsceNaZdjecie = document.getElementById('wygenerowaneZdjecie');
+                html2canvas(document.getElementById('wygenerowanaStrona'), { width: 900 }).then(function (canvas) {
+                    miejsceNaZdjecie.innerHTML = "";
+                    var img = new Image();
+                    img.src = canvas.toDataURL("image/png");
+                    img.width = 900;
+                    img.height = 440;
+                    miejsceNaZdjecie.append(img);
+                });
+            }
+            else {
+                var miejsceNaZdjecie = document.getElementById('wygenerowaneZdjecie');
                 miejsceNaZdjecie.innerHTML = "";
-                var img = new Image();
-                img.src = canvas.toDataURL("image/png");
-                img.width = 900;
-                img.height = 440;
-                miejsceNaZdjecie.append(img);
-            });
+            }
+
+            // Sprawdź czy trzeba aktywować przewijanie tekstu
+            const przewijak = document.getElementById('przewijak');
+            const stacjab = przewijak.querySelector('#stacjab');
+            const tekstSzerokosc = stacjab.scrollWidth;
+            const kontenerSzerokosc = przewijak.offsetWidth;
+
+            if (stacjab.scrollWidth > przewijak.offsetWidth) {
+                // Oblicz czas trwania animacji w zależności od szerokości tekstu i kontenera
+                const animacjaCzas = (tekstSzerokosc / kontenerSzerokosc) * 5; // 10s to początkowy czas trwania
+
+                // Ustaw czas trwania animacji
+                stacjab.style.animation = `przewijanie ${animacjaCzas}s linear infinite`;
+            }
+
             var classa = document.getElementById('boxtekst');
             classa.classList.add("animacja-migmig");
+
         })
         .catch(error => {
             console.error('Błąd pobierania szablonu:', error);
